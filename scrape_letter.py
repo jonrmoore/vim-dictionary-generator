4738a39130b96c175524b66d611aa904c4e303c1
Jon Moore <jonrmoore@protonmail.com>
2019-09-02 19:37:22 -0500

    removing temporary html files

--- scrape_letter.py
+++ scrape_letter.py
@@ -1,9 +1,12 @@
 import sys
+# import requests
 from bs4 import BeautifulSoup
 
 def extract_data_from_file(filename):
     return BeautifulSoup(open(filename), 'html.parser')
 
+# r = requests.get('https://www.merriam-webster.com/browse/dictionary')
+# print(r)
 soup = extract_data_from_file(sys.argv[1])
 entries = soup.find('div', class_="entries").find_all('a')
 
